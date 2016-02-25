79309d63d20375117b1d632b50ec6bc2d2c1c53c
AndyT94 <andytruong94@gmail.com>
2016-02-25 21:47:01 +0100

    auditlog class done

---
+++ ProjectEIT/src/server/AuditLog.java
@@ -0,0 +1,34 @@
+package server;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class AuditLog {
+	private static FileHandler fileTxt;
+	private final static Logger logger = Logger.getLogger( AuditLog.class.getName() );
+	
+	private AuditLog() {}
+	
+	public static void setUp() {
+		try {
+			logger.setUseParentHandlers(false);
+			
+			fileTxt = new FileHandler("server/data/Logging.txt", true);
+			SimpleFormatter formatterTxt = new SimpleFormatter();
+		    fileTxt.setFormatter(formatterTxt);
+		    logger.addHandler(fileTxt);
+
+		} catch (SecurityException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void log(Level level, String logMsg) {
+		logger.log(level, logMsg);
+	}
+}
